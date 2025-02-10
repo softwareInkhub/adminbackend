e8ec1b8f0b7b8604ab7dbebafc592d26b0ae94b1
INKHUB <inkhub@INKHUBs-Mac-mini.local>
2025-02-10 18:31:27 +0530

    Add Amplify configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
   // If your static files are in the public directory
   publicRuntimeConfig: {
     staticFolder: '/public',
